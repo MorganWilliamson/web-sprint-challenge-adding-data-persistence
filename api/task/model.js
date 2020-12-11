ebc8c83956e4060fbdd6dc805921a7b7dce18b1f
MorganWilliamson <mwilliamson7695@gmail.com>
2020-12-11 11:28:53 -0600

    All basic endpoints written. Needs fine-tuning and testing.

--- api/task/model.js
+++ api/task/model.js
@@ -1,2 +1,15 @@
 // build your `Task` model here
 const db = require('../../data/dbConfig');
+
+// We need to be able to: GET all resources, POST a new resource.
+module.exports = {
+    getAllTasks() {
+        return db('tasks');
+    },
+    addNewTask(task) {
+        return db('tasks').insert(task)
+            .then(([id]) => {
+                return db('task').where('id', id);
+            });   
+    }
+};
--- api/task/router.js
+++ api/task/router.js
@@ -1,8 +1,31 @@
 // build your `/api/tasks` router here
 const express = require('express');
-
+const Task = require('./model');
 const router = express.Router();
 
 /// ENDPOINTS ///
+// We need to be able to: GET all resources, POST a new resource.
+router.get('/', async (req, res) => {
+    try {
+        const tasks = await Task.getAllResources();
+        if(!tasks){
+            res.status(404).json({ message: "Could not find requested tasks." });
+        } else {
+            res.json(tasks);
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const contents = req.body;
+        const newTask = await Task.createResource(contents);
+        res.status(201).json(newTask);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 module.exports = router;
