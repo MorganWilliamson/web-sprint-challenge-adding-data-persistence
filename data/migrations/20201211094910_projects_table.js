0d91274818061143f17f25ad324522353e54ad3f
MorganWilliamson <mwilliamson7695@gmail.com>
2020-12-11 10:48:15 -0600

    Wrote out tables, migration ran without errors!

--- data/migrations/20201211094910_projects_table.js
+++ data/migrations/20201211094910_projects_table.js
@@ -5,17 +5,39 @@ exports.up = function(knex) {
         tbl.increments();
         tbl.text('name', 128).notNullable();
         tbl.text('description', 1000);
-        tbl.boolean('completed').notNullable().defaultTo(false);
+        tbl.boolean('completed').defaultTo(false).notNullable();
     })
     .createTable('resources', tbl => {
         tbl.increments();
         tbl.text('name', 128).unique().notNullable();
         tbl.text('description', 1000);
     })
+    .createTable('tasks', tbl => {
+        tbl.increments();
+        tbl.text('description', 1000).notNullable();
+        tbl.text('notes', 1000);
+        tbl.boolean('completed').defaultTo(false).notNullable();
+        tbl.integer('project_id')
+            .unsigned().notNullable()
+            .references('id').inTable('projects')
+            .onUpdate('restrict').onDelete('restrict');
+    })
+    .createTable('project_resources', tbl => {
+        tbl.integer('project_id')
+            .unsigned().notNullable()
+            .references('id').inTable('projects')
+            .onUpdate('restrict').onDelete('restrict');
+        tbl.integer('resource_id')
+            .unsigned().notNullable()
+            .references('id').inTable('resources')
+            .onUpdate('restrict').onDelete('restrict');
+    });
 };
 
 exports.down = function(knex) {
   return knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
     .dropTableIfExists('resources')
     .dropTableIfExists('projects');
 };
